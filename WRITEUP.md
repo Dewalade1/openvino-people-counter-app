fc2ecc50e6214fa56702e28fed1396bcd2f09b91
Dewalade Ade-onojobi <adewalade@gmail.com>
2020-05-20 23:58:59 +0100

    chore: update inference.py
    Add methods to be used for integrating and using the inference engine in main.py

--- WRITEUP.md
+++ WRITEUP.md
@@ -4,9 +4,28 @@ You can use this document as a template for providing your project write-up. How
 have a different format you prefer, feel free to use it as long as you answer all required
 questions.
 
+Acquiring and converting the model
+
+Code I used to formatt
+
+- wget http://download.tensorflow.org/models/object_detection/faster_rcnn_inception_v2_coco_2018_01_28.tar.gz
+
+- tar -xvf faster_rcnn_inception_v2_coco_2018_01_28.tar.gz
+
+- cd faster_rcnn_inception_v2_coco_2018_01_28 && ls -a
+
+-  python /opt/intel/openvino/deployment_tools/model_optimizer/mo_tf.py --input_model frozen_inference_graph.pb --tensorflow_object_detection_api_pipeline_config pipeline.config --reverse_input_channels --tensorflow_use_custom_operations_config /opt/intel/openvino/deployment_tools/model_optimizer/extensions/front/tf/faster_rcnn_support.json
+
 ## Explaining Custom Layers
 
+- cd .. && python main.py
+
 The process behind converting custom layers involves...
+
+Unsupported layers (faster rcnn)
+
+Unsupported layers (mask rcnn):
+ ['proposals', 'detection_output', 'SecondStageBoxPredictor_1/ResizeBilinear']
 
 Some of the potential reasons for handling custom layers are...
 
@@ -20,6 +39,10 @@ The difference between model accuracy pre- and post-conversion was...
 The size of the model pre- and post-conversion was...
 
 The inference time of the model pre- and post-conversion was...
+
+IR generation execution time = 167.69 seconds
+
+Model precision = FP32. This was chosen to ensure maximum accuracy in the model. However, this caused the model to be heavier than the lowest precision value INT16 by 
 
 ## Assess Model Use Cases
 
--- inference.py
+++ inference.py
@@ -27,41 +27,94 @@ import sys
 import logging as log
 from openvino.inference_engine import IENetwork, IECore
 
+# MODEL = "/home/workspace/model/frozen_inference_graph.xml"
+# CPU_EXTENSION = "/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so"
+
 
 class Network:
     """
-    Load and configure inference plugins for the specified target devices 
+    Load and configure inference plugins for the specified target devices
     and performs synchronous and asynchronous modes for the specified infer requests.
     """
 
     def __init__(self):
         ### TODO: Initialize any class variables desired ###
+        self.network = None
+        self.core = None
+        self.input_blob = None
+        self.output_blob = None
+        self.exec_network = None
+        self.infer_request = None
 
-    def load_model(self):
+
+    def load_model(self, model, device = "CPU", cpu_extension = None):
+        '''
+        Load the model given IR files. Defaults device = CPU.
+        Asynchronous requests made within.
+        '''
         ### TODO: Load the model ###
+        model_xml = model
+        model_bin = os.path.splitext(model_xml)[0] + ".bin"
+
+        self.core = IECore()
+        self.network = IENetwork(model = model_xml, weights = model_bin)
+
         ### TODO: Check for supported layers ###
+        supported_layers = core.query_network(self.network, device)
+
+        unsupported_layers = [l for l in self.network.layers.keys() if l not in supported_layers]
+        if len(unsupported_layers) != 0:
+            print("[Inference Engine] Unsupported layers found: \n {}".format(unsupported_layers))
+            print("[Inference Engine] Check to see if unsupported extensions are available to IECore")
+            print("[Inference Engine] If not available, add as custom layers")
+            exit(1)
+
         ### TODO: Add any necessary extensions ###
+        if cpu_extension and "CPU" in device:
+            self.core.addextension(cpu_extension, device)
+
         ### TODO: Return the loaded inference plugin ###
         ### Note: You may need to update the function parameters. ###
+
+        self_exec_network = self.core.load_network(self.network, device)
+        print("[Inference Engine] IR successfully loaded into Inference Engine")
+
+        # Get input layer
+        self.input_blob = next(iter(self.network.inputs))
+        self.output_blob = next(iter(self.network.outputs))
+
         return
 
     def get_input_shape(self):
+        '''
+        This gets the input shape of the network
+        '''
         ### TODO: Return the shape of the input layer ###
-        return
+        input_shape = self.network.inputs[self.input_blob].shape
+        print("[Inference Engine] Input shape found!")
+        return input_shape
 
-    def exec_net(self):
+    def exec_net(self, frame):
         ### TODO: Start an asynchronous request ###
+        self.exec_network.start_async(request_id = 0, inputs={self.input_blob: image})
+
         ### TODO: Return any necessary information ###
         ### Note: You may need to update the function parameters. ###
         return
 
     def wait(self):
         ### TODO: Wait for the request to be complete. ###
+        status = self.exec_network.requests[0].wait(-1)
+
         ### TODO: Return any necessary information ###
         ### Note: You may need to update the function parameters. ###
-        return
+        return status
 
     def get_output(self):
+        '''
+        Returns a list of the results for the output layer of the network.
+        '''
         ### TODO: Extract and return the output results
         ### Note: You may need to update the function parameters. ###
-        return
+        return self.exec_network.requests[0].outputs[self.output_blob]
+        
